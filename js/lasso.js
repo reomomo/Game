e32dceb9d51727f58b612db6e8765a1afa51d317
reomomo <reomomo31672@gmail.com>
2023-08-28 12:09:27 +0000

    ゲーム完成

--- js/lasso.js
+++ js/lasso.js
@@ -22,8 +22,7 @@ for (i = 1; i <= 25; i++) {
 }
 
 const lassoesOpacity = [...Array(25)].map((_, i) => i + 1);
-let copy = lassoesOpacity;
-
+let copy = [...lassoesOpacity];
 switchBtnReset.addEventListener('click', () => {
   for (i = 1; i <= 25; i++) {
     if (changeElement(2) == 0) {
@@ -39,34 +38,37 @@ switchBtnReset.addEventListener('click', () => {
   $('.lasso').css({
     'opacity': '0'
   });
+
+  copy = [...lassoesOpacity];
+
   document.getElementById("bigLasso").removeAttribute("disabled");
   document.getElementById("smallLasso").removeAttribute("disabled");
 }, false);
 
 switchBtnSmallLasso.addEventListener('click', () => {
   let selectedSmall = [...Array(3)].map(() =>
-  copy.splice(Math.floor(Math.random() * lassoesOpacity.length), 1)[0]);
-  console.log(copy.length);
+  copy.splice(Math.floor(Math.random() * copy.length), 1)[0]);
+  console.log(selectedSmall);
   selectedSmall.forEach((e) => {
     $(`.l${e}`).css({
       'opacity': '0.6',
       'width': '90%',
     });
   });
+
   document.getElementById("bigLasso").setAttribute("disabled", true);
   document.getElementById("smallLasso").setAttribute("disabled", true);
 });
 
 const lassoBigOpacity = [1,2,3,4,6,7,8,9,11,12,13,14,16,17,18,19];
+
 switchBtnBigLasso.addEventListener('click', () => {
   let selectedBig = lassoBigOpacity[Math.floor(Math.random() * lassoBigOpacity.length)];
   $(`.l${selectedBig}`).css({
     'opacity': '0.6',
     'width': '180%',
   });
+
   document.getElementById("bigLasso").setAttribute("disabled", true);
   document.getElementById("smallLasso").setAttribute("disabled", true);
 });
-
-
-// lassoesOpacityが減っていく問題を解決する！
